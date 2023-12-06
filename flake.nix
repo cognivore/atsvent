0c7d85835736e46cd1257348469a9c5add46b929
cognivore <66186054+cognivore@users.noreply.github.com>
2023-12-06 23:08:05 +0000

    Add ATS2

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs";
+    };
+    outputs = { self, nixpkgs }: 
+    let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+        devShell.x86_64-linux = pkgs.mkShell {
+            buildInputs = [ pkgs.ats2 ];
+        };
+    };
+}
